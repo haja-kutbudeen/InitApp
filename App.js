6530fab86661c05d41a09af46e2c35dad6f41a78
Haja <hajakutbudeen@gmail.com>
2022-09-06 20:07:33 +0300

    Login

--- App.js
+++ App.js
@@ -1,29 +1,67 @@
-import React from 'react'
-import { View, TouchableNativeFeedback, Text, StyleSheet } from 'react-native'
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
 
-const Home = (props) => {
-   return (
-      <View style = {styles.container}>
-         <TouchableNativeFeedback>
-            <Text style = {styles.text}>
-               Submit
-            </Text>
-         </TouchableNativeFeedback>
-      </View>
-   )
+class Inputs extends Component {
+   state = {
+      email: '',
+      password: ''
+   }
+   handleEmail = (text) => {
+      this.setState({ email: text })
+   }
+   handlePassword = (text) => {
+      this.setState({ password: text })
+   }
+   login = (email, pass) => {
+      alert('email: ' + email + ' password: ' + pass)
+   }
+   render() {
+      return (
+         <View style = {styles.container}>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Email"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handleEmail}/>
+            
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Password"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+            
+            <TouchableOpacity
+               style = {styles.submitButton}
+               onPress = {
+                  () => this.login(this.state.email, this.state.password)
+               }>
+               <Text style = {styles.submitButtonText}> Submit </Text>
+            </TouchableOpacity>
+         </View>
+      )
+   }
 }
-export default Home
+export default Inputs
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
    container: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      flex: 1
+      paddingTop: 23
    },
-   text: {
-      borderWidth: 1,
-      padding: 25,
-      borderColor: 'green',
-      backgroundColor: 'red'
+   input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#7a42f4',
+      borderWidth: 1
+   },
+   submitButton: {
+      backgroundColor: '#7a42f4',
+      padding: 10,
+      margin: 15,
+      height: 40,
+   },
+   submitButtonText: {
+      color: 'white'
    }
 })
