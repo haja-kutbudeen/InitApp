20e6aea90792cab171b9827d36a6473551c8a829
Haja <hajakutbudeen@gmail.com>
2022-09-06 19:55:25 +0300

    Touchable Button

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react'
-import { Button } from 'react-native' // Widgets
+import React from 'react'
+import { TouchableOpacity, StyleSheet, View, Text } from 'react-native'
 
 const App = () => {
-   const handlePress = () => false
    return (
-      <Button
-         onPress = {handlePress}
-         title = "Red button!"
-         color = "red"
-      />
+      <View style = {styles.container}>
+         <TouchableOpacity>
+            <Text style = {styles.text}>
+               Button
+            </Text>
+         </TouchableOpacity>
+      </View>
    )
 }
 export default App
 
-
-// Button: Touchable opacity
+const styles = StyleSheet.create ({
+   container: {
+      alignItems: 'center',
+      flex: 1,
+      justifyContent: 'center',
+   },
+   text: {
+      borderWidth: 1,
+      padding: 25,
+      borderColor: 'black',
+      backgroundColor: 'red',
+   }
+})
