106b2f7a5fe8fcda3cdbf53e5a0aac80c8dab0fd
Haja <hajakutbudeen@gmail.com>
2022-09-08 19:26:43 +0300

    Switch

--- App.js
+++ App.js
@@ -1,50 +1,25 @@
-import React, { Component } from 'react';
-import { Text, Image, View, StyleSheet, ScrollView } from 'react-native';
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import SwitchExample from './switch_example.js'
 
-class ScrollViewExample extends Component {
-   state = {
-      names: [
-         {'name': 'Ben', 'id': 1},
-         {'name': 'Susan', 'id': 2},
-         {'name': 'Robert', 'id': 3},
-         {'name': 'Mary', 'id': 4},
-         {'name': 'Daniel', 'id': 5},
-         {'name': 'Laura', 'id': 6},
-         {'name': 'John', 'id': 7},
-         {'name': 'Debra', 'id': 8},
-         {'name': 'Aron', 'id': 9},
-         {'name': 'Ann', 'id': 10},
-         {'name': 'Steve', 'id': 11},
-         {'name': 'Olivia', 'id': 12}
-      ]
+export default class HomeContainer extends Component {
+   constructor() {
+      super();
+      this.state = {
+         switch1Value: false,
+      }
+   }
+   toggleSwitch1 = (value) => {
+      this.setState({switch1Value: value})
+      console.log('Switch 1 is: ' + value)
    }
    render() {
       return (
          <View>
-            <ScrollView>
-               {
-                  this.state.names.map((item, index) => (
-                     <View key = {item.id} style = {styles.item}>
-                        <Text>{item.name}</Text>
-                     </View>
-                  ))
-               }
-            </ScrollView>
+            <SwitchExample
+            toggleSwitch1 = {this.toggleSwitch1}
+            switch1Value = {this.state.switch1Value}/>
          </View>
-      )
+      );
    }
 }
-export default ScrollViewExample
-
-const styles = StyleSheet.create ({
-   item: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: 30,
-      margin: 2,
-      borderColor: '#2a4944',
-      borderWidth: 1,
-      backgroundColor: '#d2f7f1'
-   }
-})
