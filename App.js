ea73ede0a18957cb1d27aa2c443642f1ce2606d2
Haja <hajakutbudeen@gmail.com>
2022-09-09 12:18:00 +0300

    Pending

--- App.js
+++ App.js
@@ -1,52 +1,25 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, ScrollView, View, Platform} from 'react-native';
-import {examples} from './SliderExample';
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import SwitchExample from './switch_example.js'
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <ScrollView
-        style={styles.scrollView}
-        contentContainerStyle={styles.container}>
-        <Text testID="testTextId" style={styles.title}>
-          {'<Slider />'}
-        </Text>
-        {examples
-          .filter(e => !e.platform || e.platform === Platform.OS)
-          .map((e, i) => (
-            <View key={`slider${i}`} style={styles.sliderWidget}>
-              <Text style={styles.instructions}>{e.title}</Text>
-              {e.render()}
-            </View>
-          ))}
-      </ScrollView>
-    );
-  }
+export default class HomeContainer extends Component {
+   constructor() {
+      super();
+      this.state = {
+         switch1Value: false,
+      }
+   }
+   toggleSwitch1 = (value) => {
+      this.setState({switch1Value: value})
+      console.log('Switch 1 is: ' + value)
+   }
+   render() {
+      return (
+         <View>
+            <SwitchExample
+            toggleSwitch1 = {this.toggleSwitch1}
+            switch1Value = {this.state.switch1Value}/>
+         </View>
+      );
+   }
 }
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: '#F5FCFF',
-  },
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 20,
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    width: '100%',
-    marginVertical: 20,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-    fontSize: 20,
-  },
-  sliderWidget: {
-    marginVertical: 30,
-  },
-});
